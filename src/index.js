ff07b309925c47b145dacad417636a5095311aef
aman-godara <amangodara.auth@gmail.com>
2024-07-12 20:33:15 +0530

    Install react, Add preset-react to babel loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
 import getGreeting from "./greet";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 console.log(getGreeting("World!"));
