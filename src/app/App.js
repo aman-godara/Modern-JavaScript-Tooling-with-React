41f2d8f7747f7b0c99a3f287b19468022bfe4ff6
aman-godara <amangodara.auth@gmail.com>
2024-07-12 22:45:28 +0530

    Install react-hot-loader, addhot loading to App component, add dev:react-hot script

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { hot } from "react-hot-loader";
 
 const App = () => {
   return <h1>Hello World!</h1>;
 };
 
-export default App;
+export default hot(module)(App);
