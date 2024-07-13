d48b4cfc9bea143db620ad3063a272e2048221c4
aman-godara <amangodara.auth@gmail.com>
2024-07-13 18:54:27 +0530

    Add React.lazy which gets its separate bundle by webpack

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { hot } from "react-hot-loader";
 
+const Warning = React.lazy(() => import("./Warning"));
+
+import "./app.css";
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState(true);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Suspense fallback={null}>{state && <Warning />}</Suspense>
+    </>
+  );
 };
 
 export default hot(module)(App);
---
+++ src/app/Warning.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./warning.css";
+
+const Warning = () => {
+  return <div className="warning">This is warning</div>;
+};
+
+export default Warning;
