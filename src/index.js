52791ae466a51df9facf58db8ab7b92be50a3a81
aman-godara <amangodara.auth@gmail.com>
2024-07-14 22:57:09 +0530

    Install react-error-boundary, add ErrorBoundary to app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import App from './app/App.js';
 import getGreeting from './greet.js';
@@ -8,7 +9,15 @@ import './index.css';
 
 ReactDOM.render(
     <StrictMode>
-        <App />
+        <ErrorBoundary
+            fallback={
+                <div className="error-boundary">
+                    There was an error in the app!
+                </div>
+            }
+        >
+            <App />
+        </ErrorBoundary>
     </StrictMode>,
     document.getElementById('root')
 );
