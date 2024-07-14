5afaf718e510f95f35380abd8cb1266d76694034
aman-godara <amangodara.auth@gmail.com>
2024-07-14 16:57:08 +0530

    Add TODO: add jest into globals

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,26 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
+// TODO: add jest into globals
 export default [
     { files: ['**/*.{js,mjs,cjs,jsx}'] },
-    { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    {
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
     pluginJs.configs.recommended,
     pluginReactConfig,
     {
